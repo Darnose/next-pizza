0e430d7e3e28317e20a1981675e0956d67b7c00f
Darnose <andrey.yarovoy.2013@gmail.com>
2024-07-27 10:18:20 +0300

    create header component

---
+++ components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Drawer } from './drawer';
+export { Input } from './input';
+export { Popover } from './popover';
+export { Select } from './select';
+export { Skeleton } from './skeleton';
+export { Slider } from './slider';
+export { Dialog } from './dialog';
+export { Checkbox } from './checkbox';
+export { Button } from './button';
