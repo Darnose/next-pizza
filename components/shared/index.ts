8987707d6e6af7877d477d379e3c8f9ae32c35c8
Darnose <andrey.yarovoy.2013@gmail.com>
2024-07-31 09:51:41 +0300

    create filters + checkbox

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -4,3 +4,6 @@ export { Header } from './header';
 export { Categories } from './categories';
 export { SortPopup } from './sort-popup';
 export { TopBar } from './top-bar';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
+export { RangeSlider } from './range-slider';
