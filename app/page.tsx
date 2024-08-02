113886fa2ba8b38c80c1166d9381bd07ca4e0559
Darnose <andrey.yarovoy.2013@gmail.com>
2024-08-02 11:13:12 +0300

    create product-group-list

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Container, Filters, Title, TopBar } from '@/components/shared';
+import { ProductsGroupList } from '@/components/shared/products-group-list';
 
 export default function Home() {
   return (
@@ -8,14 +9,28 @@ export default function Home() {
       </Container>
       <TopBar />
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           {/* Фильтрация */}
           <div className="w-[250px]">
             <Filters />
           </div>
           {/* Список товаров */}
           <div className="flex-1">
-            <div className="flex flex-col gap-16"></div>
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList
+                title="Пиццы"
+                items={[
+                  {
+                    id: 1,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                ]}
+                categoryId={0}
+              />
+            </div>
           </div>
         </div>
       </Container>
