27d697fb0a87c37794b8cfea7bfeb4ab90d92b9a
Darnose <andrey.yarovoy.2013@gmail.com>
2024-08-05 17:19:26 +0300

    create scroll categories + zustand

--- app/page.tsx
+++ app/page.tsx
@@ -27,8 +27,133 @@ export default function Home() {
                       'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
                     items: [{ price: 200 }],
                   },
+                  {
+                    id: 2,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 3,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 4,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 5,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 6,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 7,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 8,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 9,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
                 ]}
-                categoryId={0}
+                categoryId={1}
+              />
+              <ProductsGroupList
+                title="Комбо"
+                items={[
+                  {
+                    id: 1,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 2,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 3,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 4,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 5,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 6,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 7,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 8,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                  {
+                    id: 9,
+                    name: 'Пицца-Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                    items: [{ price: 200 }],
+                  },
+                ]}
+                categoryId={2}
               />
             </div>
           </div>
